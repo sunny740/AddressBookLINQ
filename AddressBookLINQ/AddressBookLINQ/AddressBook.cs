308f17943bca1c31411ca0c9e26ed12265b9cf3b
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-25 22:35:08 +0530

    [Sunny] Add : Ability To Create AddressBook Services

---
+++ AddressBookLINQ/AddressBookLINQ/AddressBook.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    public class AddressBook
+    {
+        DataTable dataTable;
+        public void CreateDataTable()
+        {
+            dataTable = new DataTable("AddressBook");
+            DataColumn dtColumn;
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Int32);
+            dtColumn.ColumnName = "ID";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "FirstName";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "LastName";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "Address";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "City";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "State";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "Email";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Int64);
+            dtColumn.ColumnName = "PhoneNumber";
+            dataTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Int64);
+            dtColumn.ColumnName = "Zip";
+            dataTable.Columns.Add(dtColumn);
+
+        }
+    }
+}
---
+++ AddressBookLINQ/AddressBookLINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AddressBookLINQ
+{
+    public class  Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Using LINQ");
+            AddressBook addressBook = new AddressBook();
+            addressBook.CreateDataTable();
+
+        }
+    }
+}
